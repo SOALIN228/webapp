4aa2fb94468eee4312207e9c56f08c045519abf1
lml <soalin228@163.com>
2019-05-03 20:41:55 +0800

    订单列表结构完成

--- order/orderList/orderList.js
+++ order/orderList/orderList.js
@@ -17,6 +17,55 @@
     '  $getComment\n' +
     '</div>'
 
+  // 渲染总计菜品
+  function getTotalPrice (data) {
+    let str =
+      '<div class="product-item">\n' +
+      '  <span>...</span>\n' +
+      '  <div class="p-total-count">\n' +
+      '    总计' + data.product_count + '个菜，实付\n' +
+      '    <span class="total-price">¥' + data.total + '</span>\n' +
+      '  </div>\n' +
+      '</div>'
+    return str
+  }
+
+  // 渲染菜品
+  function getProduct (data) {
+    let list = data.product_list || []
+    list.push({ type: 'more' })
+
+    let str = ''
+    list.forEach(function (item) {
+      if (item.type === 'more') {
+        str += getTotalPrice(data)
+      } else {
+        str +=
+          '<div class="product-item">\n' +
+          item.product_name +
+          '  <div class="p-count">x\n' +
+          item.product_count +
+          '  </div>\n' +
+          '</div>'
+      }
+    })
+
+    return str
+  }
+
+  // 渲染评价按钮
+  function getComment(data){
+    let evaluation = !data.is_comment;
+    if (evaluation) {
+      return '<div class="evaluation clearfix">'+
+        '<div class="evaluation-btn">评价</div>'+
+        '</div>';
+
+    }
+
+    return '';
+  }
+
   // 渲染列表
   function initContentList (list) {
     let str = ''
@@ -24,6 +73,9 @@
       str += itemTmpl
       .replace('$poi_pic', item.poi_pic)
       .replace('$poi_name', item.poi_name)
+      .replace('$status_description', item.status_description)
+      .replace('$getProduct', getProduct(item))
+      .replace('$getComment', getComment(item))
 
     })
     $('.order-list').append(str)
