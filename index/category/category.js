1795cfd898a6c73d210eefe5e30da2f3b13160b6
lml <soalin228@163.com>
2019-05-03 12:04:14 +0800

    首页category完成

---
+++ index/category/category.js
@@ -0,0 +1,35 @@
+(function () {
+  // 类目模板字符串
+  let itemTmpl = '<div class="category-item">' +
+    '<img class="item-icon" src="$url" alt="">' +
+    '<p class="item-name">$name</p>' +
+    '</div>'
+
+  // 渲染元素
+  function initCategory () {
+    // 获取数据
+    $.get('../json/head.json', function (data) {
+      let list = data.data.primary_filter.splice(0, 8)
+      list.forEach(function (item, index) {
+        let str = itemTmpl.replace('$url', item.url)
+        .replace('$name', item.name)
+
+        $('.category-content').append(str)
+      })
+    })
+  }
+
+  // 给item添加click事件
+  function addClick () {
+    $('.category-content').on('click','.category-item', function () {
+      console.log(event.target)
+    })
+  }
+
+  function init () {
+    initCategory()
+    addClick()
+  }
+
+  init()
+})()
