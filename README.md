fc31e4bb1b3cf5ebe3b84bf50c815aaf62bbaf03
lml <soalin228@163.com>
2019-05-11 15:16:15 +0800

    订单页面使用MVC管理代码

--- README.md
+++ README.md
@@ -5,4 +5,16 @@
 2. 组件化开发模式
 3. 使用拼接字符串动态记载数据
 4. listl列表可以滚动加载数据
-5. 使用MVC设计模式来管理js代码
+5. 使用立即函数和闭关封装，代码互不污染
+6. 使用MVC设计模式来管理js代码
+
+**技巧**：
+1. 使用css做一个箭头
+
+   ```css
+   width: 10px;
+   height: 10px;
+   border-top: 1px solid #999;
+   border-right: 1px solid #999;
+   transform: rotate(45deg);
+   ```
--- order/orderList/orderList.js
+++ order/orderList/orderList.js
@@ -1,121 +1,125 @@
 (function () {
-
-  // 订单卡片模板
-  let itemTmpl =
-    '<div class="order-item">\n' +
-    '  <div class="order-item-inner">\n' +
-    '    <img src="$poi_pic" alt="" class="item-img">\n' +
-    '    <div class="item-right">\n' +
-    '      <div class="item-top">\n' +
-    '        <p class="order-name one-line">$poi_name</p>\n' +
-    '        <div class="arrow"></div>\n' +
-    '        <div class="order-state">$status_description</div>\n' +
-    '      </div>\n' +
-    '      <div class="item-bottom">$getProduct</div>\n' +
-    '    </div>\n' +
-    '  </div>\n' +
-    '  $getComment\n' +
-    '</div>'
-
-  // 渲染总计菜品
-  function getTotalPrice (data) {
-    let str =
-      '<div class="product-item">\n' +
+  let temp = {
+    itemTmpl: '<div class="order-item">\n' +
+      '  <div class="order-item-inner">\n' +
+      '    <img src="$poi_pic" alt="" class="item-img">\n' +
+      '    <div class="item-right">\n' +
+      '      <div class="item-top">\n' +
+      '        <p class="order-name one-line">$poi_name</p>\n' +
+      '        <div class="arrow"></div>\n' +
+      '        <div class="order-state">$status_description</div>\n' +
+      '      </div>\n' +
+      '      <div class="item-bottom">$getProduct</div>\n' +
+      '    </div>\n' +
+      '  </div>\n' +
+      '  $getComment\n' +
+      '</div>',
+    totalItem: '<div class="product-item">\n' +
       '  <span>...</span>\n' +
       '  <div class="p-total-count">\n' +
-      '    总计' + data.product_count + '个菜，实付\n' +
-      '    <span class="total-price">¥' + data.total + '</span>\n' +
+      '    总计$product_count个菜，实付\n' +
+      '    <span class="total-price">¥$total</span>\n' +
       '  </div>\n' +
+      '</div>',
+    productItem: '<div class="product-item">\n' +
+      '$product_name' +
+      '  <div class="p-count">x $product_count</div>\n' +
+      '</div>',
+    commentItem: '<div class="evaluation clearfix">' +
+      '<div class="evaluation-btn">评价</div>' +
       '</div>'
-    return str
   }
 
-  // 渲染菜品
-  function getProduct (data) {
-    let list = data.product_list || []
-    list.push({ type: 'more' })
+  let view = $('.order-list')
 
-    let str = ''
-    list.forEach(function (item) {
-      if (item.type === 'more') {
-        str += getTotalPrice(data)
-      } else {
-        str +=
-          '<div class="product-item">\n' +
-          item.product_name +
-          '  <div class="p-count">x\n' +
-          item.product_count +
-          '  </div>\n' +
-          '</div>'
-      }
-    })
-
-    return str
-  }
-
-  // 渲染评价按钮
-  function getComment (data) {
-    let evaluation = !data.is_comment
-    if (evaluation) {
-      return '<div class="evaluation clearfix">' +
-        '<div class="evaluation-btn">评价</div>' +
-        '</div>'
+  let model = {
+    fetch: function () {
+      return $.get('../json/orders.json')
     }
-
-    return ''
   }
 
-  // 渲染列表
-  function initContentList (list) {
-    let str = ''
-    list.forEach(function (item) {
-      str += itemTmpl
-      .replace('$poi_pic', item.poi_pic)
-      .replace('$poi_name', item.poi_name)
-      .replace('$status_description', item.status_description)
-      .replace('$getProduct', getProduct(item))
-      .replace('$getComment', getComment(item))
+  let controller = {
+    view: null,
+    model: null,
+    temp: null,
+    page: 0,
+    isLoading: false,
+    init: function (view, model, temp) {
+      this.view = view
+      this.model = model
+      this.temp = temp
+      this.getList()
+      this.bindEvents()
+    },
+    getList: function () {
+      this.page++
+      this.isLoading = true
+      this.model.fetch().then((data) => {
+        let list = data.data.digestlist || []
+        this.initContentList(list)
+        this.isLoading = false
+      })
+    },
+    initContentList: function (list) {
+      let str = ''
+      list.forEach((item) => {
+        str += this.temp.itemTmpl
+        .replace('$poi_pic', item.poi_pic)
+        .replace('$poi_name', item.poi_name)
+        .replace('$status_description', item.status_description)
+        .replace('$getProduct', this.getProduct(item))
+        .replace('$getComment', this.getComment(item))
+      })
+      this.view.append(str)
+    },
+    // 渲染菜品
+    getProduct: function (data) {
+      let list = data.product_list || []
+      list.push({ type: 'more' })
 
-    })
-    $('.order-list').append(str)
-  }
-
-  let page = 0
-  let isLoading = false
-
-  // 请求数据
-  function getList () {
-    page++
-    isLoading = true
-    $.get('../json/orders.json', function (data) {
-      let list = data.data.digestlist || []
-      initContentList(list)
-      isLoading = false
-    })
-  }
-
-  // 滚动加载
-  function addEvent () {
-    window.addEventListener('scroll', function () {
-      let clienHeight = document.documentElement.clientHeight
-      let scollHeight = document.body.scrollHeight
-      let scrollTop = document.documentElement.scrollTop || document.body.scrollTop
-      let proDis = 30
-      if ((scrollTop + clienHeight) >= (scollHeight - proDis)) {
-        if (page < 3) {
-          if (isLoading) {
-            return
-          }
-          getList()
+      let str = ''
+      list.forEach((item) => {
+        if (item.type === 'more') {
+          str += this.getTotalPrice(data)
+        } else {
+          str += this.temp.productItem
+          .replace('$product_name', item.product_name)
+          .replace('$product_count', item.product_count)
         }
+      })
+      return str
+    },
+    getTotalPrice: function (data) {
+      return this.temp.totalItem
+      .replace('$product_count', data.product_count)
+      .replace('$total', data.total)
+    },
+    // 渲染评价按钮
+    getComment: function (data) {
+      if (!data.is_comment) {
+        return this.temp.commentItem
       }
-    })
+      return ''
+    },
+    // 滚动加载
+    bindEvents: function () {
+      window.addEventListener('scroll', () => {
+          let clienHeight = document.documentElement.clientHeight // 屏幕高度
+          let scollHeight = document.body.scrollHeight // 内容高度
+          let scrollTop = document.documentElement.scrollTop || document.body.scrollTop // 滚动高度
+          let proDis = 30 // 距离底边高度
+          if ((scrollTop + clienHeight) >= (scollHeight - proDis)) {
+            if (this.page < 3) {
+              if (this.isLoading) {
+                return
+              }
+              this.getList()
+            }
+          }
+        }
+      )
+    }
   }
 
-  function init () {
-    getList()
-    addEvent()
-  }
-
-  init()
+  controller.init(view, model, temp)
 })()
